1f24ed3af7d856ba7c4d8da85b187f1f1b35dc25
surfincanoy <163241904+surfincanoy@users.noreply.github.com>
2025-08-21 11:53:24 +0800

    Delete webui4srt.bat

--- webui4srt.bat
+++
@@ -1,7 +0,0 @@
-set "SCRIPT_DIR=%~dp0"
-set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
-cd /d "%SCRIPT_DIR%"
-set "PATH=%SCRIPT_DIR%\.venv;%PATH%"
-set PYTHONPATH=.
-.venv\Scripts\python.exe -s webui4srt.py
-pause
